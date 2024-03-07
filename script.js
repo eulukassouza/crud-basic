53be4f74eeb61ab01d2929a08d8e79c4d7132dde
lukasdev <apolloboficiall@gmail.com>
2024-03-07 01:56:26 -0400

    .

--- script.js
+++ script.js
@@ -1,52 +1,20 @@
-// script.js
-let cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
-
-function saveProduct() {
-    const productName = document.getElementById('productName').value;
-    const productValue = parseFloat(document.getElementById('productValue').value);
-    const productQuantity = parseInt(document.getElementById('productQuantity').value);
-
-    if (productName && !isNaN(productValue) && !isNaN(productQuantity) && productValue > 0 && productQuantity > 0) {
-        const item = { name: productName, price: productValue, quantity: productQuantity };
-        cartItems.push(item);
-        updateCart();
-        saveToLocalStorage();
-        clearForm();
-    } else {
-        alert('Por favor, preencha todos os campos corretamente.');
+function allowDrop(event) {
+    event.preventDefault();
+  }
+  
+  function drag(event) {
+    event.dataTransfer.setData("text", event.target.id);
+  }
+  
+  function drop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text");
+    var draggedElement = document.getElementById(data);
+    var destinationList = event.target;
+  
+    // Check if the drop target is a list
+    if (destinationList.classList.contains("list")) {
+      destinationList.appendChild(draggedElement);
     }
-}
-
-function updateCart() {
-    const cartItemsElement = document.getElementById('cart-items');
-    const totalElement = document.getElementById('total');
-
-    cartItemsElement.innerHTML = '';
-    let total = 0;
-
-    cartItems.forEach(item => {
-        const li = document.createElement('li');
-        li.className = 'cart-item';
-        li.textContent = `${item.name} - R$${item.price.toFixed(2)} x ${item.quantity}`;
-        cartItemsElement.appendChild(li);
-
-        total += item.price * item.quantity;
-    });
-
-    totalElement.textContent = `Total: R$${total.toFixed(2)}`;
-}
-
-function saveProductsToLocalStorage() {
-    localStorage.setItem('savedProducts', JSON.stringify(cartItems));
-    alert('Produtos cadastrados foram salvos com sucesso!');
-}
-
-function clearForm() {
-    document.getElementById('productName').value = '';
-    document.getElementById('productValue').value = '';
-    document.getElementById('productQuantity').value = '';
-}
-
-function saveToLocalStorage() {
-    localStorage.setItem('cartItems', JSON.stringify(cartItems));
-}
+  }
+  
